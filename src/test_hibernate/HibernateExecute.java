ca660a8523452a38a32ee288c955b18be4d2973f
bineea <981344903@qq.com>
2017-11-28 23:43:29 +0800

    hibernate test

---
+++ src/test_hibernate/HibernateExecute.java
@@ -0,0 +1,33 @@
+package test_hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateExecute {
+	public static void main(String[] args)
+	{
+		HibernateExecute execute = new HibernateExecute();
+		execute.add();
+	}
+	
+	public void add()
+	{
+		Configuration conf = new Configuration().configure();
+		SessionFactory sf = conf.buildSessionFactory();
+		Session session = sf.openSession();
+		Transaction tx = session.beginTransaction();
+		User user = new User();
+		user.setId("002");
+		user.setName("test");
+		user.setAge(0);
+		user.setMobile("13012341234");
+		user.setEmail("123123@qq.com");
+		user.setPassword("123123");
+		session.save(user);
+		tx.commit();
+		session.close();
+		sf.close();
+	}
+}

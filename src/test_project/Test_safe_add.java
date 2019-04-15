107a2a90af4cc1cefa556db7e918dd7144f3c896
bineea <981344903@qq.com>
2019-04-15 13:39:18 +0800

    多线程安全累加

--- src/test_project/Test_safe_add.java
+++ src/test_project/Test_safe_add.java
@@ -1,9 +1,33 @@
 package test_project;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Test_safe_add {
 
-    public static void main(String[] args) {
-        System.out.println("hello world。。。你好");
+    public static AtomicInteger atomic = new AtomicInteger(0);
+    private static final int thread_count = 20;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] Threads = new Thread[thread_count];
+        for (int i=0;i<thread_count;i++) {
+            int num = i;
+            Threads[i] = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (int a=0;a<1000;a++) {
+                        atomic.incrementAndGet();
+                        System.out.println("当前执行线程【"+ num +"】，累加次数【"+a+"】");
+                    }
+                }
+            });
+            Threads[i].start();
+        }
+
+        while(atomic.intValue() != 20000) {
+            continue;
+        }
+        System.out.println(atomic);
     }
 
 }

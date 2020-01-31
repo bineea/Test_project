c1c838f3fedeab6619b856f1ab6e2cf5d1b95a16
bineea <33547731+bineea@users.noreply.github.com>
2020-01-31 17:05:28 +0800

    Parallel Scavenge收集器触发GC的条件

--- src/test_project/jvm/GC/Test_gc_MinorGC.java
+++ src/test_project/jvm/GC/Test_gc_MinorGC.java
@@ -6,7 +6,10 @@ package test_project.jvm.GC;
 //-XX:survivorRatio=8设定新生代中Eden区与一个Survivor区的空间比例为8:1
 
 
-//Parallel Scavenge收集器没有触发GC!!!!!什么鬼？？？
+//Parallel Scavenge收集器没有触发GC!!!!!
+//通过对比发现，当整个新生代剩余的空间无法存放某个对象时，Parallel Scavenge/Parallel Old中该对象会直接进入老年代。而如果整个新生代剩余的空间可以存放但只是Eden区空间不足，则会尝试一次Minor GC。
+//而对于Serial/Serial Old当发现Eden区不足以存放对象时，就进行一次Minor GC
+
 public class Test_gc_MinorGC {
 	
 	private static final int _1MB = 1024 * 1024;
@@ -16,9 +19,9 @@ public class Test_gc_MinorGC {
 		allocation1 = new byte[2 * _1MB];
 		allocation2 = new byte[2 * _1MB];
 		allocation3 = new byte[2 * _1MB];
-		//System.gc();
-		allocation4 = new byte[4 * _1MB];
 		
+//		allocation4 = new byte[1 * _1MB];
+//		allocation4 = new byte[4 * _1MB];
 	}
 	
 	public static void main(String[] args) {

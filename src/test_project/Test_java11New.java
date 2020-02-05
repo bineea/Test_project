6ae4a7c9d1c4ae7d03b6525ddf4e0ac621927072
bineea <33547731+bineea@users.noreply.github.com>
2020-02-05 19:32:15 +0800

    Create Test_java11New.java

---
+++ src/test_project/Test_java11New.java
@@ -0,0 +1,14 @@
+package test_project;
+
+import java.util.List;
+
+public class Test_java11New {
+
+    public static void main(String[] args) {
+        var list = List.of(1,2,3,4,5,6,7,8,9,0);
+        System.out.println("takeWhile:");
+        list.stream().takeWhile(n -> n<5).forEach(n -> System.out.print(n));
+        System.out.println("\ndropWhile:");
+        list.stream().dropWhile(n -> n<5).forEach(n -> System.out.print(n));
+    }
+}

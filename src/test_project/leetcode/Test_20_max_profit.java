28ea8cbed613972b71658858ef15fc392a4367d6
bineea <33547731+bineea@users.noreply.github.com>
2020-03-09 21:18:33 +0800

    优化

--- src/test_project/leetcode/Test_20_max_profit.java
+++ src/test_project/leetcode/Test_20_max_profit.java
@@ -26,6 +26,14 @@ public class Test_20_max_profit {
 	public int maxProfit_FINAL(int[] prices) {
 		int res = 0;
 		
+		int min_price = Integer.MAX_VALUE;
+		
+		for(int i=0; i<prices.length; i++) {
+			if(prices[i] < min_price)
+				min_price = prices[i];
+			res = Math.max(res, prices[i] - min_price);
+		}
+		
 		return res;
 	}
 	

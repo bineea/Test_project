0d0ec84ae070605bf0c7b201a91354c6484c2673
bineea <33547731+bineea@users.noreply.github.com>
2020-08-27 23:26:13 +0800

    整理方法

--- src/test_project/Test_unsafe.java
+++ src/test_project/Test_unsafe.java
@@ -20,36 +20,37 @@ public class Test_unsafe {
             e.printStackTrace();
         }
     }
-
-    public static void main(String[] args) throws NoSuchFieldException {
-
-        int j = 1;
-        System.out.println(j << (31 - Integer.numberOfLeadingZeros(3)));
-        System.out.println(j * 3);
-        System.out.println(j << (31 - Integer.numberOfLeadingZeros(4)));
-        System.out.println(j * 4);
-        System.out.println(j << (31 - Integer.numberOfLeadingZeros(5)));
-        System.out.println(j * 5);
-
-        /*内部类
-        final Test_unsafe test_unsafe = new Test_unsafe();
-        class Test_inner {
-            public void test_inner2outer() {
-                System.out.println("test_inner2outer:" + test_unsafe.x);
-            }
-        }*/
-
-        final Test_unsafe test_unsafe = new Test_unsafe();
-
-        /*Unsafe事项cas操作
+    
+    /**
+     * Unsafe获取数据元素
+     */
+    public void getArrayObject() {
+        Integer[] testInt = new Integer[] {1,2,3};
+        int x_scale = UNSAFE.arrayIndexScale(testInt.getClass());
+        long base_offset = UNSAFE.arrayBaseOffset(testInt.getClass());
+        Integer second = (Integer) UNSAFE.getObject(testInt, (2 - 1) * x_scale + base_offset);
+        System.out.println("testInt数组中的第二个元素为："+second);
+    }
+    
+    /**
+     * Unsafe实现cas操作
+     * @throws SecurityException 
+     * @throws NoSuchFieldException 
+     */
+    public void sampleCasOperate(Test_unsafe test_unsafe) throws NoSuchFieldException, SecurityException {
         long x_offset = UNSAFE.objectFieldOffset(test_unsafe.getClass().getDeclaredField("x"));
         int tmp = test_unsafe.x;
         UNSAFE.compareAndSwapInt(test_unsafe, x_offset, tmp, tmp+=1);
         System.out.println("x offset :"+x_offset);
-        System.out.println("tmp:"+tmp);*/
-
-        //Unsafe实现具体cas操作
-        
+        System.out.println("tmp:"+tmp);
+    }
+    
+    /**
+     * Unsafe实现具体cas操作
+     * @throws SecurityException 
+     * @throws NoSuchFieldException 
+     */
+    public void casOperate(Test_unsafe test_unsafe) throws NoSuchFieldException, SecurityException {
         //获取非静态属性Field在对象实例中的偏移量，读写对象的非静态属性时会用到这个偏移量。
         final long x_offset = UNSAFE.objectFieldOffset(test_unsafe.getClass().getDeclaredField("x"));
 
@@ -92,6 +93,30 @@ public class Test_unsafe {
                 }
             }
         }).start();
+    }
+
+    public static void main(String[] args) throws NoSuchFieldException {
+
+        int j = 1;
+        System.out.println(j << (31 - Integer.numberOfLeadingZeros(3)));
+        System.out.println(j * 3);
+        System.out.println(j << (31 - Integer.numberOfLeadingZeros(4)));
+        System.out.println(j * 4);
+        System.out.println(j << (31 - Integer.numberOfLeadingZeros(5)));
+        System.out.println(j * 5);
+
+        /*内部类
+        final Test_unsafe test_unsafe = new Test_unsafe();
+        class Test_inner {
+            public void test_inner2outer() {
+                System.out.println("test_inner2outer:" + test_unsafe.x);
+            }
+        }*/
+
+        final Test_unsafe test_unsafe = new Test_unsafe();
+        test_unsafe.getArrayObject();
+        test_unsafe.sampleCasOperate(test_unsafe);
+        test_unsafe.casOperate(test_unsafe);
     }
 
 }

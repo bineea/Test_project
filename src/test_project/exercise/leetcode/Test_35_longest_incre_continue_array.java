f1b1e8b1e8301987576ff23ac1b0bc897aa9df4c
bineea <33547731+bineea@users.noreply.github.com>
2021-01-24 22:27:36 +0800

    最长连续递增子序列

---
+++ src/test_project/exercise/leetcode/Test_35_longest_incre_continue_array.java
@@ -0,0 +1,34 @@
+package test_project.exercise.leetcode;
+
+import java.util.Stack;
+
+/**
+ * 最长连续递增
+ * @author binee
+ *
+ */
+public class Test_35_longest_incre_continue_array {
+
+	public static void main(String[] args) {
+		Test_35_longest_incre_continue_array test = new Test_35_longest_incre_continue_array();
+		System.out.println(test.findLengthOfLCIS_BL(new int[] {1,3,5,4,7}));
+		System.out.println(test.findLengthOfLCIS_BL(new int[] {2,2,2,2,2}));
+	}
+	
+	public int findLengthOfLCIS_BL(int[] nums) {
+		
+		int result = 0;
+		Stack<Integer> stack = new Stack<Integer>();
+		for(int i=0; i<nums.length; i++) {
+			if(stack.empty() || stack.lastElement() < nums[i]) {
+				stack.add(nums[i]);
+				result = Math.max(result, stack.size());
+			} else {
+				stack.clear();
+				stack.add(nums[i]);
+			}
+		}
+		
+		return result;
+	}
+}

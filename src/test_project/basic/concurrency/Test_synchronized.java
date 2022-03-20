25cd47c3fdb431f469224e24d3cb966ed03adc7b
guowenbin9 <guowenbin9@jd.com>
2022-03-20 22:20:44 +0800

    wait方法阻塞当前线程

--- src/test_project/basic/concurrency/Test_synchronized.java
+++ src/test_project/basic/concurrency/Test_synchronized.java
@@ -2,6 +2,27 @@ package test_project.basic.concurrency;
 
 public class Test_synchronized {
 
+	private Object lock = new Object();
 	
-	//TODO 
+	public void testWait() {
+		
+		for (int i=0; i<5; i++) {
+			synchronized(lock) {
+				try {
+					System.out.println("执行"+i+"开始，时间戳："+System.currentTimeMillis());
+					lock.wait(1000);
+					System.out.println("执行"+i+"完成，时间戳："+System.currentTimeMillis());
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Test_synchronized test = new Test_synchronized();
+		test.testWait();
+	}
 }

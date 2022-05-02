4bd1d53d9e43ec7133898c2b9409a730e5073a66
bineea <33547731+bineea@users.noreply.github.com>
2022-05-02 15:58:39 +0800

    调整

--- src/test_project/exercise/leetcode/Test_36_sort_array_parity.java
+++ src/test_project/exercise/leetcode/Test_36_sort_array_parity.java
@@ -1,7 +1,6 @@
 package test_project.exercise.leetcode;
 
 import java.util.Arrays;
-import java.util.LinkedList;
 
 /**
  * 给你一个整数数组 nums，将 nums 中的的所有偶数元素移动到数组的前面，后跟所有奇数元素。
@@ -18,24 +17,66 @@ public class Test_36_sort_array_parity {
 
 	public static void main(String[] args) {
 		Test_36_sort_array_parity test = new Test_36_sort_array_parity();
-		String res = Arrays.toString(test.sortArrayByParity(new int[] {3,1,2,4}));
-		System.out.println(res);
+		String res1 = Arrays.toString(test.sortArrayByParity_1(new int[] {3,1,2,4}));
+		System.out.println(res1);
+		String res2 = Arrays.toString(test.sortArrayByParity_1(new int[] {0,1}));
+		System.out.println(res2);
 	}
 	
-    public int[] sortArrayByParity(int[] nums) {
-        LinkedList<Integer> link = new LinkedList<Integer>();
+	/**
+	 * @param nums
+	 * @return
+	 */
+    public int[] sortArrayByParity_1(int[] nums) {
+        int[] resNums = new int[nums.length];
+        
+    	// 奇数下标
+    	int oddIndex = nums.length -1;
+    	// 偶数下标
+    	int evenIndex = 0;
+        
         for(int i=0;i<nums.length;i++){
             if(nums[i] % 2 == 0){
-                link.push(nums[i]);
+            	resNums[evenIndex] = nums[i];
+            	evenIndex++;
             }else{
-                link.offer(nums[i]);
-            }
+            	resNums[oddIndex] = nums[i];
+            	oddIndex--;           }
         }
-        Integer[] temp = link.toArray(new Integer[nums.length]);
-        int[] res = new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-        	res[i] = temp[i];
-        }
-        return res;
+
+        return resNums;
     }
+    
+    /**
+     * 
+     * @param nums
+     * @return
+     */
+    public int[] sortArrayByParity_2(int[] nums) {
+    	
+    	// 奇数下标
+    	int oddIndex = 0;
+    	// 偶数下标
+    	int evenIndex = nums.length -1;
+    	for(int i=nums.length - 1; i>=0; i--){
+    		if(nums[i] % 2 == 0){
+    			evenIndex = i;
+            } else {
+            	continue;
+            }
+    		
+    		for (int x=oddIndex; x<evenIndex; x++) {
+    			if(nums[x] % 2 != 0) {
+    				oddIndex = x;
+    				break;
+    			}
+    		}
+    		int temp = nums[i];
+    		nums[i] = nums[oddIndex];
+    		nums[oddIndex] = temp;
+    	}
+    	return nums;
+    }
+    
+    
 }

ff013124d23e5b37d38d79a7f8851d0016383dd5
bineea <33547731+bineea@users.noreply.github.com>
2022-05-02 18:49:39 +0800

    遍历树的全部节点

---
+++ src/test_project/exercise/leetcode/Test_37_get_tree_all_elements.java
@@ -0,0 +1,30 @@
+package test_project.exercise.leetcode;
+
+import java.util.List;
+
+public class Test_37_get_tree_all_elements {
+
+	public static void main(String[] args) {
+		//TODO 
+	}
+	
+	public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+		//TODO 
+		return null;
+    }
+}
+
+
+//Definition for a binary tree node.
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}

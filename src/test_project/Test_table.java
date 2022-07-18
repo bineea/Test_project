c454b65e5ab38bc9278443a0ccf068b95ade67f5
guowb1 <guowb1@lenovo.com>
2022-07-18 11:51:02 +0800

    guava中table的key和value均不允许为null

---
+++ src/test_project/Test_table.java
@@ -0,0 +1,14 @@
+package test_project;
+
+import com.google.common.collect.HashBasedTable;
+import com.google.common.collect.Table;
+
+public class Test_table {
+
+	
+	public static void main(String[] args) {
+		Table<Integer, String, Object> priorityDataTable = HashBasedTable.create();
+		priorityDataTable.put(1, "1", null);
+		System.out.println(String.valueOf(priorityDataTable.get(1, "1")));
+	}
+}

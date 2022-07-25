59a8b14c027a13dcfd23640af9455287e7c3312e
guowb1 <guowb1@lenovo.com>
2022-07-25 11:13:00 +0800

    table的key和value都不允许为null

--- src/test_project/Test_table.java
+++ src/test_project/Test_table.java
@@ -8,6 +8,8 @@ public class Test_table {
 	
 	public static void main(String[] args) {
 		Table<Integer, String, Object> priorityDataTable = HashBasedTable.create();
+		priorityDataTable.put(1, "1", "");
+		System.out.println(String.valueOf(priorityDataTable.get(1, "1")));
 		priorityDataTable.put(1, "1", null);
 		System.out.println(String.valueOf(priorityDataTable.get(1, "1")));
 	}
